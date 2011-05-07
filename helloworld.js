fcd96cf710ec807b4ebc11436601d17ad0607b67
Christian Ranz <git@christianranz.com>
2011-05-07 19:03:31 +0000

    deleted all the stuff

--- helloworld.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req, res){
-    
-    res.writeHead(200);
-    res.write('Hello World');
-    res.end();
-    
-}).listen(process.env.C9_PORT,'0.0.0.0');
